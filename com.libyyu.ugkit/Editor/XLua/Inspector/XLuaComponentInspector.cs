804905c76c0a11209ebef584e0d23b03ce6c7e35
libyyu <389465209@qq.com>
2026-02-08 16:51:38 +0800

    xlua

---
+++ com.libyyu.ugkit/Editor/XLua/Inspector/XLuaComponentInspector.cs
@@ -0,0 +1,25 @@
+﻿#if ENABLE_UGKIT_TENCENT_XLUA
+using UGKit.Editor;
+using UGKit.XLua.Runtime;
+using UnityEditor;
+
+namespace UGKit.XLua.Editor
+{
+    [CustomEditor(typeof(XLuaComponent))]
+    internal sealed class XLuaComponentInspector : ComponentTypeComponentInspector
+    {
+        protected override void RefreshTypeNames()
+        {
+            RefreshComponentTypeNames(typeof(IXLuaManager));
+        }
+
+        protected override void Enable()
+        {
+            base.Enable();
+
+            // m_Timeout = serializedObject.FindProperty("m_Timeout");
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+#endif
